d1783fc1f37660468f2ba6af17d757ec21bf6e21
Rubens Martins <rubens.martins@tst.jus.br>
2024-05-29 15:48:57 -0300

    highlightedState animates between transitions and animations transfered to separate file

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -15,29 +15,3 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     animate(200)
   ])
 ])
-
-
-export const shownStateTrigger = trigger('shownState', [
-  transition(':enter', [
-    style({
-      opacity: 0
-    }),
-    animate(300, style({
-      opacity: 1
-    }))
-  ]),
-  transition(':leave', [
-    animate(300, style({
-      opacity: 0
-    }))
-  ])
-])
-
-
-export const checkButtonTrigger = trigger('checkButton', [
-  transition('* => checked', [
-    animate('400ms ease-in', style({
-      transform: 'scale(0.4)'
-    }))
-  ])
-])
